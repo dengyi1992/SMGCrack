58c91c0f95da1ac68fb9c44c497b2a8a2bb4ec44
978548481@qq.com <d7577355>
2016-05-17 16:48:23 +0800

    sdtdsrt

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,10 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
-router.get('/login', function (req, res) {
+router.get('/kxummsjfl93/denglu.asp', function (req, res) {
     //生成密码的 md5 值
     var md5 = crypto.createHash('md5'),
-        name = md5.update(req.query.zh).digest('hex');
+        name = md5.update(req.query.u).digest('hex');
     res.end(name)
 });
 router.get('/loginNumber', function (req, res) {
